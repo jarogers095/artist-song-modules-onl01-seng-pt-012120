97a0552791ec888f1b76911008041e40be80601b
James Rogers <jarogers095@gmail.com>
2020-02-18 03:11:16 +0000

    Automatically backed up by Learn

--- lib/concerns/memorable.rb
+++ lib/concerns/memorable.rb
@@ -1,10 +1,10 @@
 module Memorable
   module ClassMethods
-  def reset_all()
-    self.all.clear
-  end
-  
-  def count()
-    self.all.count
-  end
+    def reset_all()
+      self.all.clear
+    end
+    
+    def count()
+      self.all.count
+    end
 end
